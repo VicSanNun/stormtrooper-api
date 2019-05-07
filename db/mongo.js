2bfd520ae4c6de431ae5269fb6a354a7304e0d6e
Victor Nunes <victornunes470@gmail.com>
2019-05-07 01:10:40 -0300

    Refatorando o arquivo de conexão com o banco, para utilizar o config.json

--- db/mongo.js
+++ db/mongo.js
@@ -1,14 +1,15 @@
 'use strict';
 
 const mongojs = require('mongojs');
+const config = require('config');
 const debug = require('debug')('livro_nodejs:db');
 
 function _connection(){
-    const user = "";
-    const pass = "";
-    const server = "localhost";
-    const port = "21017";
-    const database = "livro_nodejs";
+    const user = config.get('mongo.user');
+    const pass = config.get('mongo.pass');
+    const server = config.get('mongo.server');
+    const port = config.get('mongo.port');
+    const database = config.get('mongo.database');
     const auth = user ? user + ":" + pass + "@" : "";
 
     return "mongodb://"+auth+server+':'+port+'/'+database;
