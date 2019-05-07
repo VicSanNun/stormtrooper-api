cd40d3b8e771c325ea33c788d4e80cc124478a17
Victor Nunes <victornunes470@gmail.com>
2019-05-07 01:21:13 -0300

    Refatorando o arquivo de Model para utilizar o Banco de Dados

--- models/StormtrooperModel.js
+++ models/StormtrooperModel.js
@@ -1,10 +1,27 @@
 "use strict";
 
 class StormtrooperModel{
-    find(query, callback){}
-    create(data, callback){}
-    update(_id, data, callback){}
-    remove(_id, callback){}
+    constructor(mongo){
+        this.mongo = mongo;
+    }
+    find(query, callback){
+        this.mongo.collection('stormtroopers').find(query, callback);
+    }
+    findOne(_id, callback){
+        const query = {_id: this.mongo.ObjectId(_id)};
+        this.mongo.collection('stormtroopers').findOne(query, callback);
+    }
+    create(data, callback){
+        this.mongo.collection('stormtroopers').insert(data, callback);
+    }
+    update(_id, data, callback){
+        const query = {_id: this.mongo.ObjectId(_id)};
+        this.mongo.collection('stormtrooper').update(query, data, callback);
+    }
+    remove(_id, callback){
+        const query = {_id: this.mongo.ObjectId(_id)};
+        this.mongo.collection('stormtroopers').remove(query, callback);
+    }
 }
 
-module.exports = new StormtrooperModel();
+module.exports = (mongo) => new StormtrooperModel(mongo);
