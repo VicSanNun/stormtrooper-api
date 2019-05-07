440939c64ee3169858a5bd0c9fa59842722884e9
Victor Nunes <victornunes470@gmail.com>
2019-05-06 22:16:17 -0300

    Adicionando a Classe StormtrooperModel

---
+++ models/StormtrooperModel.js
@@ -0,0 +1,10 @@
+"use strict";
+
+class StormtrooperModel{
+    find(query, callback){}
+    create(data, callback){}
+    update(_id, data, callback){}
+    remove(_id, callback){}
+}
+
+module.exports = new StormtrooperModel();
