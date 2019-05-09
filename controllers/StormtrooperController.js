7080b0505a05082c1f09a8b2eb6685296fb2130f
Victor Nunes <victornunes470@gmail.com>
2019-05-09 14:52:55 -0300

    Refatorando o Controller para utilizar Promises

--- controllers/StormtrooperController.js
+++ controllers/StormtrooperController.js
@@ -1,50 +1,66 @@
 "use strict";
 
 const debug = require('debug');
+const Promise = require('bluebird');
 
 class StormtrooperController{
     constructor(StormtrooperModel){
-        this.model = StormtrooperModel;
+        this.model = Promise.promisifyAll(StormtrooperModel);
+    }
+
+    handleNotFound(data){
+        if(!data){
+            const err = new Error('Not Found');
+            err.status = 404;
+            throw err;
+        }
+        return data;
     }
 
     getAll(req, res, next){
-        this.model.find({}, (err, data)=>{
-            if(err) return(next(err));
-            res.json(data);
-        });
+        this.model.findAsync({})
+            .then((data)=>{
+                res.json(data);
+            })
+            .catch(next);
     }
 
     getById(req, res, next){
         const _id = req.params._id;
-        this.model.findOne(_id, (err, data)=>{
-            if(err) return(next(err));
-            res.json(data);
-        });
+        this.model.findOneAsync(_id)
+            .then(this.handleNotFound)
+            .then(data=>{
+                res.json(data)
+            })
+            .catch(next);
     }
 
     create(req, res, next){
         const body = req.body;
-        this.model.create(body, (err, data)=>{
-            if(err) return(next(err));
-            res.json(data);
-        });
+        this.model.createAsync(body)
+            .then((err, data)=>{
+                res.json(data);
+            })
+            .catch(next);
     }
 
     update(req, res, next){
         const _id = req.params._id;
         const body = req.body;
-        this.model.update(_id, (err, data)=>{
-            if(err) return(next(err));
-            res.json(data);
-        });
+        this.model.updateAsync(_id, body)
+            .then((err, data)=>{
+                res.json(data);
+            })
+            .catch(next);
     }
 
     remove(req, res, next){
         const _id = req.params._id;
-        this.model.remove(_id, (err, data)=>{
-            if(err) return(next(err));
-            res.json(data);
-        });
+        this.model.removeAsync(_id)
+            .then((err, data)=>{
+                res.json(data);
+            })
+            .catch(next);
     }
 }
 
