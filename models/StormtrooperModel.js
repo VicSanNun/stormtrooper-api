d3f087eabbad1ad44b54c20e72e7931406d17b28
Victor Nunes <victornunes470@gmail.com>
2019-05-09 14:54:28 -0300

    Feature: Utilizando o Pattern DAO

--- models/StormtrooperModel.js
+++ models/StormtrooperModel.js
@@ -1,27 +1,42 @@
 "use strict";
 
-class StormtrooperModel{
-    constructor(mongo){
-        this.mongo = mongo;
+class StormtrooperDAO{
+    constructor(model){
+        this.model = model;
     }
     find(query, callback){
-        this.mongo.collection('stormtroopers').find(query, callback);
+        this.model.find(query).exec(callback);
     }
     findOne(_id, callback){
-        const query = {_id: this.mongo.ObjectId(_id)};
-        this.mongo.collection('stormtroopers').findOne(query, callback);
+        const query = {_id: _id};
+        this.model.findOne(query).exec(callback);
     }
     create(data, callback){
-        this.mongo.collection('stormtroopers').insert(data, callback);
+        const model = new this.model(data);
+        model.save((err, result)=>{
+            callback(err, result);
+        });
     }
     update(_id, data, callback){
-        const query = {_id: this.mongo.ObjectId(_id)};
-        this.mongo.collection('stormtrooper').update(query, data, callback);
+        const query = {_id: _id};
+        this.model.update(query, data).exec((err, result)=>{
+            callback(err, result);
+        });
     }
     remove(_id, callback){
-        const query = {_id: this.mongo.ObjectId(_id)};
-        this.mongo.collection('stormtroopers').remove(query, callback);
+        const query = {_id: _id};
+        this.model.remove(query).exec((err, result)=>{
+            callback(err, result);
+        });
     }
 }
 
-module.exports = (mongo) => new StormtrooperModel(mongo);
+module.exports = (mongoose) => {
+    const Stormtrooper = mongoose.model('Stormtrooper', {
+        name: String,
+        nickname: String,
+        divisions: [String],
+        patent: String
+    });
+    return new StormtrooperDAO(Stormtrooper);
+};
